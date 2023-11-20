9885cb9e24e4f7a643012b17d3736025da3ff043
Luna <142634281+KittyLuna@users.noreply.github.com>
2023-11-20 17:41:26 -0500

    Create cardpicker.lua

---
+++ cardpicker.lua
@@ -0,0 +1,11 @@
+local queue_on_teleport = queue_on_teleport or syn.queue_on_teleport or fluxus.queue_on_teleport or function(...) return ... end
+
+game.Players.LocalPlayer.OnTeleport:Connect(function(state)
+    if state ~= Enum.TeleportState.Started and state ~= Enum.TeleportState.InProgress then return end
+    queue_on_teleport([[
+        repeat task.wait() until game:IsLoaded()
+        wait(2)
+        if getgenv().Executed then return end -- avoid multiple executions
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/KittyLuna/Anime-Adventures/main/Anime%20Adventures%20Halloween%20Event%20Cardpicker%202023))()
+    ]])
+end)
