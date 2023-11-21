82b7d5547c67448d5ccf70b3d99cc60868f2f059
Luna <142634281+KittyLuna@users.noreply.github.com>
2023-11-20 21:34:58 -0500

    Update cardpicker.lua

--- cardpicker.lua
+++ cardpicker.lua
@@ -10,7 +10,6 @@ game.Players.LocalPlayer.OnTeleport:Connect(function(state)
 
         wait(2)
             
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/ultrahub7/Ultra-Hub/main/Main.lua"))()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/KittyLuna/Anime-Adventures/main/cardpickerbeta"))()
     ]])
 
