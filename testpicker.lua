c2706ef2c0768fc087b7695a705c0dcb10322d10
Luna <142634281+KittyLuna@users.noreply.github.com>
2023-11-22 08:54:51 -0500

    Delete testpicker.lua

--- testpicker.lua
+++
@@ -1,9 +0,0 @@
-task.spawn(function()
-    local queue_on_teleport =
-queue_on_teleport or
-    syn and
-        syn.queue_on_teleport [[
-       repeat task.wait() until game:IsLoaded() wait(5) print("Server Hopped or rejoined")
-
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/KittyLuna/Anime-Adventures/main/cardpickerbeta"))()]]
-end)
